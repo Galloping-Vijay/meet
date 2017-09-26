7ffb13f6cdf33d6a356c7e7c964370c37daa1371
wjf19940211 <1937832819@qq.com>
2017-09-26 14:00:21 +0800

    测试

--- app/home/controller/Test.php
+++ app/home/controller/Test.php
@@ -28,14 +28,32 @@ class Test extends Base
         if(!empty($config)) $this->options=array_merge($this->options,$config);
        $app = new Application($this->options);
        $js = $app->js;
-       $wxconfig = $js->config($APIs, $debug = true, $beta = false, $json = false);
+       $wxconfig = $js->config($APIs, $debug = true, $beta = false, $json = true);
        pr($wxconfig);
+        $wxconfig = Array
+        (
+            'debug' => 1,
+            'beta' => '',
+            'appId' => 'wxf31792477fe34e63',
+            'nonceStr' => '4OT9JTUBjy',
+            'timestamp' => '1506404953',
+            'url' => 'http://www.meetoyou.com/home/test/index',
+            'signature' => '1855727310348ea245b586e348ac09f3dcd01b2d',
+            'jsApiList' => ["onMenuShareTimeline",//分享到朋友圈
+                "onMenuShareAppMessage",//分享给朋友
+                "onMenuShareQQ",//分享到QQ
+                "onMenuShareWeibo",//分享到腾讯微博
+                "onMenuShareQZone",//分享到QQ空间
+            ]
+        );
+        $this->assign('wxconfig', $wxconfig);
+        return $this->fetch('test:index');
     }
 
     public function ceshi()
     {
-       $text = new Text();
-       $data = $text->reply('测试一下');
+        $text = new Text();
+        $data = $text->reply('测试一下');
         pr($data);
     }
 }

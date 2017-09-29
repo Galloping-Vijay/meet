c5d253d0c987ff864d96735a29245df0b8b473fe
wjf19940211 <1937832819@qq.com>
2017-09-29 21:04:15 +0800

    设置前台菜单,指定菜单模块id

--- app/admin/model/Menu.php
+++ app/admin/model/Menu.php
@@ -17,8 +17,18 @@ use think\Model;
  */
 class Menu extends Model
 {
-	public function news()
-	{
-		return $this->hasMany('News','news_columnid')->bind('menu_name');
-	}
+    public function news()
+    {
+        return $this->hasMany('News', 'news_columnid')->bind('menu_name');
+    }
+
+    /**
+     * 关联模块
+     * Author: wjf <1937832819@qq.com>
+     * @return \think\model\relation\BelongsTo
+     */
+    public function module()
+    {
+        return $this->belongsTo('Module', 'menu_moduleid', 'id');
+    }
 }
